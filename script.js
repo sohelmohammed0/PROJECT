2151df9b2cb909ea9334b3f6f31db21d187ee297
sohelmohammed0 <sohel88004329@gmail.com>
2025-02-27 17:24:23 +0530

    Updated background color to red

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-fetch('config.json')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('stage').textContent = data.stage;
-
-        // Change background color based on stage
-        if (data.stage === "Development") {
-            document.body.style.backgroundColor = "blue";
-        } else if (data.stage === "Testing") {
-            document.body.style.backgroundColor = "green";
-        } else if (data.stage === "Production") {
-            document.body.style.backgroundColor = "red";
-        }
-    })
-    .catch(error => console.error('Error loading config.json:', error));
+function changeColor() {
+    document.body.style.backgroundColor = "green";
+    document.getElementById("version").innerText = "1.1";
+}
